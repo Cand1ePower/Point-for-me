61a40477835dcde553b170dd137b01e6e5f84b5c
CandlePower <candlepower1112@gmail.com>
2025-03-08 20:49:01 +0800

    First Try of pfm

---
+++ src/main/java/cdpw/pfm/PointForMeClient.java
@@ -0,0 +1,10 @@
+package cdpw.pfm;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PointForMeClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/cdpw/pfm/PointForMeDataGenerator.java
@@ -0,0 +1,11 @@
+package cdpw.pfm;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PointForMeDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
